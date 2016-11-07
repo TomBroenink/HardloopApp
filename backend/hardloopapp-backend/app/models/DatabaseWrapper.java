2156021c9abf837e80065b282b8d281729fb0f70
Tom Meijer <tommeijer95@hotmail.com>
2016-11-07 13:00:30 +0100

    no message

--- backend/hardloopapp-backend/app/models/DatabaseWrapper.java
+++ backend/hardloopapp-backend/app/models/DatabaseWrapper.java
@@ -18,114 +18,187 @@ class DatabaseWrapper{
     }
     
     public JSONArray getAllMonitors() throws Exception{
-		return executeQuery("select monitors.id, personalData_id, accessLevel, firstName, lastName, phoneNumber, username from monitors join personaldata on monitors.personalData_id = personaldata.id");
+    	String sql = "select monitors.id, personalData_id, accessLevel, firstName, lastName, phoneNumber, username from monitors join personaldata on monitors.personalData_id = personaldata.id";
+		return executeQuery(sql, "Could not retrieve monitors.");
 	}
     
     public JSONArray getAllClients() throws Exception{
-		return executeQuery("select clients.id, personalData_id, firstName, lastName, phoneNumber, username from clients join personaldata on clients.personalData_id = personaldata.id");
+    	String sql = "select clients.id, personalData_id, firstName, lastName, phoneNumber, username from clients join personaldata on clients.personalData_id = personaldata.id";
+		return executeQuery(sql, "Could not retrieve clients.");
 	}
     
     public int registerClient(String firstName, String lastName, String phoneNumber, String username, String password) throws Exception{
 		String[] values = {firstName, lastName, phoneNumber, username, password};
 		String sql = addValues("insert into personaldata values(0,", values);
-		sql += "insert into clients values(0, last_insert_id());";
-		return executeInsertReturnId(sql, "Could not register client.");
+		sql += "; insert into clients values(0, last_insert_id());";
+		return executeInsertReturnId(sql, "Could not register client.", db.getConnection(), false, true, true);
 	}
     
     public int registerMonitor(String firstName, String lastName, String phoneNumber, String username, String password, String accessLevel) throws Exception{
 		String[] values = {firstName, lastName, phoneNumber, username, password};
 		String sql = addValues("insert into personaldata values(0,", values);
-		sql += "insert into monitors values(0, last_insert_id(), '" + accessLevel + "');";
-		return executeInsertReturnId(sql, "Could not register monitor.");
+		sql += "; insert into monitors values(0, last_insert_id(), '" + accessLevel + "');";
+		return executeInsertReturnId(sql, "Could not register monitor.", db.getConnection(), false, true, true);
 	}
     
     public void removeUser(String personalDataId) throws Exception{
-		executeUpdate("delete from personaldata where id = " + personalDataId, "No user found to remove.");
+		executeUpdate("delete from personaldata where id = " + personalDataId, "No user found to remove.", db.getConnection(), true, true, true);
 	}
     
     public void assignClientToMonitor(String monitorId, String clientId, String monitorNumber) throws Exception{
 		String[] values = {monitorId, clientId, monitorNumber};
 		String sql = addValues("insert into monitors_clients values(", values);
-		executeUpdate(sql, "Could not assign client to monitor.");
+		executeUpdate(sql, "Could not assign client to monitor.", db.getConnection(), true, true, true);
 	}
     
     public int createRunSchema(String name, String description) throws Exception{
 		String[] values = {name, description};
 		String sql = addValues("insert into runschemas values(0,", values);
-		return executeInsertReturnId(sql, "Could not create run schema.");
+		return executeInsertReturnId(sql, "Could not create run schema.", db.getConnection(), true, true, true);
 	}
     
     public void assignRunSchemaToClient(String clientId, String runSchemaId) throws Exception{
 		String[] values = {clientId, runSchemaId};
 		String sql = addValues("insert into clients_runschemas values(", values);
-		executeUpdate(sql, "Could not assign run schema to client.");
+		executeUpdate(sql, "Could not assign run schema to client.", db.getConnection(), true, true, true);
 	}
     
     public void assignRunToRunSchema(String runSchemaId, String runId, String day, String time) throws Exception{
 		String[] values = {runSchemaId, runId, day, time};
 		String sql = addValues("insert into runschemas_runs values(", values);
-		executeUpdate(sql, "Could not assign run to run schema.");
+		executeUpdate(sql, "Could not assign run to run schema.", db.getConnection(), true, true, true);
 	}
     
-    public int createRun(String name, String description, String routeId) throws Exception{
-		String[] values = {name, description, routeId};
-		String sql = addValues("insert into runs values(0,", values);
-		return executeInsertReturnId(sql, "Could not create run.");
+    public int createRun(String name, String description, String distance, JSONArray route) throws Exception{
+    	Connection conn = null;
+    	ResultSet rs = null;
+		Statement statement = null;
+    	int runId = 0;
+    	try{
+    		conn = db.getConnection();
+    		String sql = addValues("insert into routes values(0,", new String[]{distance}) + ";";
+    		int routeId = executeInsertReturnId(sql, "Could not create route.", conn, false, false, false);
+    		sql = "insert into coordinates values(0,";
+    		String[] values = new String[2];
+    		for(int i = 0; i < route.size(); i++){
+    			JSONArray coordinates = (JSONArray) route.get(i);
+    			values[0] = (String) coordinates.get(0);
+    			values[1] = (String) coordinates.get(1);
+    			sql = addValues(sql, values);
+    			if(i < (route.size() - 1)){
+    				sql += ", (0,";
+    			}
+    		}
+    		executeUpdate(sql + ";", "Could not create coordinates.", conn, false, false, false);
+    		statement = conn.createStatement();
+    		rs = statement.executeQuery("select last_insert_id() as endId, row_count() as insertCount;");
+    		rs.next();
+    		int endId = rs.getInt("endId");
+    		System.out.println("EndId: " + endId);
+    		System.out.println("InsertCount: " + rs.getInt("insertCount"));
+    		int startId = endId - rs.getInt("insertCount") + 1;
+    		sql = "insert into routes_coordinates values(";
+    		for(int i = startId; i <= endId; i++){
+				sql += routeId + ", " + i + ", " + (i - startId + 1) + ")";
+				if(i <= (endId - 1)){
+    				sql += ", (";
+    			}
+			}
+    		System.out.println(sql);
+    		executeUpdate(sql + ";", "Could not create coordinates for route.", conn, false, false, false);
+    		sql = addValues("insert into runs values(0,", new String[]{name, description, String.valueOf(routeId)}) + ";";
+    		runId = executeInsertReturnId(sql, "Could not create run.", conn, false, false, true);
+    		return runId;
+		}
+    	catch(Exception e){
+    		conn.rollback();
+    		e.printStackTrace();
+    		throw e;
+    	}
+    	finally{
+    		closeResultSet(rs);
+    		closeStatement(statement);
+			closeConnection(conn);
+		}
 	}
     
-    public void createRoute(String distance, String[][] coordinates) throws Exception{
-    	
-	}
-    
-    private int executeInsertReturnId(String sql, String errorMessage) throws Exception{
-		Connection conn = null;
-		try{
-			conn = db.getConnection();
-			Statement statement = conn.createStatement();
-			boolean results = statement.execute(sql + " select last_insert_id() as id;");
-			int count = 0;
-			int id = 0;
-			while(results || count != -1){
-				if(results){
-					ResultSet rs = statement.getResultSet();
-					rs.next();
-			        id = rs.getInt("id");
-			    }
-				else{
-					count = statement.getUpdateCount();
-					if(count == 0){
-						throw new Exception(errorMessage);
-					}
-			    }
-			    results = statement.getMoreResults();
+    private int executeInsertReturnId(String sql, String errorMessage, Connection conn, boolean autoCommit, boolean closeConnection, boolean commit) throws Exception{
+		ResultSet rs = null;
+		Statement statement = null;
+    	try{
+			conn.setAutoCommit(autoCommit);
+			statement = conn.createStatement();
+			if(statement.executeUpdate(sql) == 0){
+				throw new Exception("Update count == 0.");
+			}
+			rs = statement.executeQuery("select last_insert_id() as id;");
+			rs.next();
+			int id = rs.getInt("id");
+			if(!autoCommit && commit){
+				conn.commit();
 			}
 			return id;
 		}
+		catch(Exception e){
+			if(!autoCommit){
+				conn.rollback();;
+			}
+			e.printStackTrace();
+			throw new Exception(errorMessage);
+		}
 		finally{
-			closeConnection(conn);
+			closeResultSet(rs);
+			closeStatement(statement);
+			if(closeConnection){
+				closeConnection(conn);
+			}
 		}
 	}
     
-    private void executeUpdate(String sql, String errorMessage) throws Exception{
-		Connection conn = null;
-		try{
-			conn = db.getConnection();
-		    if(conn.createStatement().executeUpdate(sql) == 0){
-		    	throw new Exception(errorMessage);
+    private void executeUpdate(String sql, String errorMessage, Connection conn, boolean autoCommit, boolean closeConnection, boolean commit) throws Exception{
+    	Statement statement = null;
+    	try{
+			conn.setAutoCommit(autoCommit);
+			statement = conn.createStatement();
+		    if(statement.executeUpdate(sql) == 0){
+		    	throw new Exception("Update count == 0.");
 		    }
+		    if(!autoCommit && commit){
+				conn.commit();
+			}
+		}
+		catch(Exception e){
+			if(!autoCommit){
+				conn.rollback();;
+			}
+			e.printStackTrace();
+			throw new Exception(errorMessage);
 		}
 		finally{
-			closeConnection(conn);
+			closeStatement(statement);
+			if(closeConnection){
+				closeConnection(conn);
+			}
 		}
 	}
 	
-	private JSONArray executeQuery(String sql) throws Exception{
+	private JSONArray executeQuery(String sql, String errorMessage) throws Exception{
 		Connection conn = null;
+		ResultSet rs = null;
+		Statement statement = null;
 		try{
-		    conn = db.getConnection();
-		    return resultToJSONArray(conn.createStatement().executeQuery(sql));
+			conn = db.getConnection();
+			statement = conn.createStatement();
+			rs = statement.executeQuery(sql);
+		    return resultToJSONArray(rs);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			throw new Exception(errorMessage);
 		}
 		finally{
+			closeResultSet(rs);
+			closeStatement(statement);
 			closeConnection(conn);
 		}
 	}
@@ -164,7 +237,7 @@ class DatabaseWrapper{
 				sql += "'" + arg + "'"; 
 			}
 			if(i == (args.length - 1)){
-				sql += ");";
+				sql += ")";
 			}
 			else{
 				sql += ",";
@@ -177,6 +250,28 @@ class DatabaseWrapper{
 		if(conn != null){
 			try{
 				conn.close();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void closeResultSet(ResultSet rs){
+		if(rs != null){
+			try{
+				rs.close();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void closeStatement(Statement statement){
+		if(statement != null){
+			try{
+				statement.close();
 			}
 			catch(Exception e){
 				e.printStackTrace();
