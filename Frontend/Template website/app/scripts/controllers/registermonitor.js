8d826eb7fc1fe32ccb9b1a940f79f850ae12bd68
Maarten <maartenaj@hotmail.com>
2016-11-15 14:29:09 +0100

    add register websocket send

--- Frontend/Template website/app/scripts/controllers/registermonitor.js
+++ Frontend/Template website/app/scripts/controllers/registermonitor.js
@@ -10,20 +10,35 @@
 
 angular.module('yapp')
 	.controller('RegisterMonitorCtrl', function($scope) {
-		
+		var response;
+		var jsonFile;
+
+		var webSocket = new WebSocket("ws://localhost:9002/ws");
+		$scope.message = "hoi";
+
 		$scope.submit = function() {
 			$scope.createJson();
 			console.log(jsonFile);
 		}
-		var jsonFile;
+		webSocket.onmessage = function(event) {
+			response = JSON.parse(event.data);
+			if (response.responseAction == "registerMonitor") {
+				$scope.message = "Register complete!";
+			}
+			$scope.$apply();
+		}
 		$scope.createJson = function() {
 			jsonFile = {
-				"firstName" : $scope.firstName,
-				"lastName" : $scope.lastName,
-				"phoneNumber" : $scope.phoneNumber,
-				"username" : $scope.userName,
-				"password" : $scope.passWord,
-				"profile" : $scope.profile
+				"requestAction": "registerMonitor",
+				"person": {
+				"firstName": $scope.firstName,
+				"lastName": $scope.lastName,
+				"phoneNumber": $scope.phoneNumber,
+				"username": $scope.username,
+				"password": $scope.password,
+				"accessLevel": "1"
+				}
 			}
+			webSocket.send(JSON.stringify(jsonFile));
 		}
-});
+	});
